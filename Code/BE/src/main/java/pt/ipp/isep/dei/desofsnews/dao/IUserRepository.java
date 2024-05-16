82d1309ad501ccbf3c88f60793694f04d12c70c4
edumi8 <eduardoapsoua@gmail.com>
2024-05-16 23:38:43 +0100

    Added dockerfiles (#33)
    * Added dockerfiles
    
    * Update dockerfile
    
    * Update dockerfile
    
    * Update on path

--- Code/BE/src/main/java/pt/ipp/isep/dei/desofsnews/dao/IUserRepository.java
+++
@@ -1,9 +0,0 @@
-package pt.ipp.isep.dei.desofsnews.dao;
-
-import java.util.UUID;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface IUserRepository extends CrudRepository<User, UUID> {
-
-}
