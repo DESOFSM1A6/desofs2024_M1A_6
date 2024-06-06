457d0faadb2d8baa5e3f193f6f1ca8a75fbb95ec
torrespedro19 <1191526@isep.ipp.pt>
2024-06-06 19:15:48 +0100

    Cypress test for login with every role to check permissions

---
+++ Code/FE/desofs-news/src/app/Components/logout/logout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { KeycloakService } from '../../Services/keycloak-init.factory';
+
+@Component({
+  selector: 'app-logout',
+  templateUrl: './logout.component.html',
+  styleUrls: ['./logout.component.css']
+})
+export class LogoutComponent implements OnInit {
+  constructor(private keycloakService: KeycloakService) {}
+
+  ngOnInit() {
+    this.keycloakService.logout();
+  }
+}
